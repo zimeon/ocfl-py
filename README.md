4263e7c79db68e55b8448f43e565f40b9517b168
Simeon Warner <simeon.warner@cornell.edu>
2018-09-09 11:42:16 +0100

    Add link to spec

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ocfl-py - An OCFL implementation in Python
+
+**WORK IN PROGRESS - NO GUARANTEE, NO MONEY BACK :-)**
+
+Attempts to follow [current draft of OCFL specification](https://ocfl.io/draft/spec/)
--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
                  "Intended Audience :: Developers",
                  "Operating System :: OS Independent",
                  "Programming Language :: Python",
+                 "Programming Language :: Python :: 2.7",
                  "Programming Language :: Python :: 3.5",
                  "Programming Language :: Python :: 3.6",
                  "Topic :: Internet :: WWW/HTTP",
