4dca72f659c2758e7ed38b17eed499d69fa11512
Simeon Warner <simeon.warner@cornell.edu>
2018-10-08 18:29:59 -0400

    Test init

--- tests/test_object.py
+++ tests/test_object.py
@@ -6,7 +6,23 @@ from ocfl.object import Object, remove_first_directory
 class TestAll(unittest.TestCase):
     """TestAll class to run tests."""
 
-    def test01_remove_first_directory(self):
+    def test01_init(self):
+        """Test Object init."""
+        oo = Object()
+        self.assertEqual(oo.identifier, None)
+        self.assertEqual(oo.digest_algorithm, 'sha512')
+        self.assertEqual(oo.skips, set())
+        self.assertEqual(oo.ocfl_version, 'draft')
+        self.assertEqual(oo.fixity, None)
+        oo = Object(identifier='a:b', digest_algorithm='sha1', skips=['1', '2'],
+                    ocfl_version='0.9.9', fixity=['md5', 'crc16'])
+        self.assertEqual(oo.identifier, 'a:b')
+        self.assertEqual(oo.digest_algorithm, 'sha1')
+        self.assertEqual(oo.skips, set(('1', '2')))
+        self.assertEqual(oo.ocfl_version, '0.9.9')
+        self.assertEqual(oo.fixity, ['md5', 'crc16'])
+
+    def test90_remove_first_directory(self):
         """Test encode."""
         self.assertEqual(remove_first_directory(''), '')
         self.assertEqual(remove_first_directory('a'), '')
