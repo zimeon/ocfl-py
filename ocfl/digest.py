0031cea06088fa6fe83811217c60435606ca3101
Simeon Warner <simeon.warner@cornell.edu>
2019-10-03 18:49:27 -0400

    Add more digest algorithms

--- ocfl/digest.py
+++ ocfl/digest.py
@@ -18,12 +18,16 @@ def _file_digest(filename, digester):
 def file_digest(filename, digest_type='sha512'):
     """Digest of digest_type for file filename.
 
-    Supports digest_type values:
-        'md5', 'sha1', 'sha256', 'sha512' -- per OCFL spec
-        'sha512-spec-ex' -- truncated sha512 useful for examples
+    Supports digest_type values from OCFL spec:
+        'md5', 'sha1', 'sha256', 'sha512', 'blake2b-512'
+    and from OCFL extensions
+        'blake2b-160', 'blake2b-256', 'blake2b-384'
+    and a truncated sha512 useful for examples
+        'sha512-spec-ex'
 
     Raises an exception if the digest_type is not supported.
     """
+    # From spec
     if digest_type == 'sha512':
         return _file_digest(filename, hashlib.sha512())
     elif digest_type == 'sha256':
@@ -32,8 +36,17 @@ def file_digest(filename, digest_type='sha512'):
         return _file_digest(filename, hashlib.sha1())
     elif digest_type == 'md5':
         return _file_digest(filename, hashlib.md5())
+    elif digest_type == 'blake2b-512':
+        return _file_digest(filename, hashlib.blake2b())
+    # From extensions
+    elif digest_type == 'blake2b-160':
+        return _file_digest(filename, hashlib.blake2b(digest_size=20))
+    elif digest_type == 'blake2b-256':
+        return _file_digest(filename, hashlib.blake2b(digest_size=32))
+    elif digest_type == 'blake2b-384':
+        return _file_digest(filename, hashlib.blake2b(digest_size=48))
+    # Specification examples: 6 chars ... 3 chars
     elif digest_type == 'sha512-spec-ex':
-        # Specification examples: 6 chars ... 3 chars
         d = _file_digest(filename, hashlib.sha512())
         return d[:6] + '...' + d[-3:]
     else:
--- tests/test_digest.py
+++ tests/test_digest.py
@@ -16,6 +16,14 @@ class TestAll(unittest.TestCase):
                          'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855')
         self.assertEqual(file_digest('tests/testdata/files/empty', 'sha512'),
                          'cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e')
+        self.assertEqual(file_digest('tests/testdata/files/empty', 'blake2b-512'),
+                         '786a02f742015903c6c6fd852552d272912f4740e15847618a86e217f71f5419d25e1031afee585313896444934eb04b903a685b1448b755d56f701afe9be2ce')
+        self.assertEqual(file_digest('tests/testdata/files/empty', 'blake2b-384'),
+                         'b32811423377f52d7862286ee1a72ee540524380fda1724a6f25d7978c6fd3244a6caf0498812673c5e05ef583825100')
+        self.assertEqual(file_digest('tests/testdata/files/empty', 'blake2b-256'),
+                         '0e5751c026e543b2e8ab2eb06099daa1d1e5df47778f7787faab45cdf12fe3a8')
+        self.assertEqual(file_digest('tests/testdata/files/empty', 'blake2b-160'),
+                         '3345524abf6bbe1809449224b5972c41790b6cf2')
         self.assertEqual(file_digest('tests/testdata/files/empty', 'sha512-spec-ex'),
                          'cf83e1...a3e')
         self.assertRaises(Exception, file_digest, 'tests/testdata/files/empty', 'bad-digest-type')
