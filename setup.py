15ae46a4df41403ebd81656cd920edb13677bf29
Simeon Warner <simeon.warner@cornell.edu>
2022-04-19 16:03:52 -0400

    Remove old travis links, update for 1.1

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
     install_requires=[
         'bagit>=1.8.1',
         'dateutils>=0.6.6',
-        'fs>2.4.0',
+        'fs>2.4.13',
         'fs_s3fs>=1.1.1',
         'pairtree>=0.8.1'
     ],
