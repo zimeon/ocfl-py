0c0295ef48622ed58e5427f44743bf0973cd7e7b
Simeon Warner <simeon.warner@cornell.edu>
2024-10-26 20:03:12 +0100

    Still WIP

---
+++ ocfl/inventory.py
@@ -0,0 +1,463 @@
+"""OCFL Inventory.
+
+The storage mechanism for the inventory data is the python dict()
+structure resulting from reading the inventory JSON file and suitable
+for writing an inventory JSON file. Here we provide convenient
+property based access to read and set this data safely without
+needing such intimate knowledge of the JSON format.
+"""
+import copy
+import json
+import os.path
+import re
+
+
+def version_number_from_directory(vdir):
+    """Get the version number from the version directory name."""
+    return int(vdir.lstrip('v'))
+
+
+class InventoryException(Exception):
+    """Exception class for Inventory class."""
+
+
+class Inventory():  # pylint: disable=too-many-public-methods
+    """Class wrapping OCFL inventory data.
+
+    In general, the property accessors and methods return None if the
+    attribute is a string that is not set in the underlying data, else
+    the value if it is. In the cases that the normal return value would
+    be an array or a dict, then and empty array or empty dict are returned
+    if not present in the underlying data.
+    """
+
+    def __init__(self, data=None, filepath=None):
+        """Initialize Inventory object.
+
+        Argument:
+            data: If not None (default) the must be either an Inventory
+                object or inventory data to initialise from. In either
+                case the underlying data is deep copied to create a separate
+                new object.
+            filepath: If not None then the string file path from which to
+                read a JSON inventory file to initialize from.
+        """
+        if data is None:
+            if filepath is None:
+                self._data = {}
+            else:
+                # FIXME - Use pyfs and add error handling
+                with open(filepath, 'r', encoding="utf-8") as fh:
+                    self._data = json.load(fh)
+        elif isinstance(data, Inventory):
+            self._data = copy.deepcopy(data.data)
+        elif isinstance(data, dict):
+            self._data = copy.deepcopy(data)
+        else:
+            raise InventoryException("Bad data type supplied to Inventory() creator, " + str(type(data)))
+
+    @property
+    def data(self):
+        """Access internal data."""
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        """Set internal data to supplied value."""
+        self._data = value
+
+    @property
+    def spec_version(self):
+        """Get specification version from the conformance declaration."""
+        decl = self._data.get("type")
+        m = re.match(r'''https://ocfl.io/(\d+.\d)/spec/#inventory''', decl)
+        if m:
+            return m.group(1)
+        return None
+
+    @spec_version.setter
+    def spec_version(self, value):
+        """Set specification version in the conformance declaration."""
+        self._data["type"] = "https://ocfl.io/" + value + "/spec/#inventory"
+
+    @property
+    def digest_algorithm(self):
+        """Get digest algorithm."""
+        return self._data.get("digestAlgorithm")
+
+    @digest_algorithm.setter
+    def digest_algorithm(self, value):
+        """Set digest algorithm."""
+        self._data["digestAlgorithm"] = value
+
+    @property
+    def identifier(self):
+        """Get object identifier."""
+        return self._data.get("id")
+
+    @identifier.setter
+    def identifier(self, value):
+        """Set object identifier."""
+        self._data["id"] = value
+
+    @property
+    def head(self):
+        """Get head version directory."""
+        return self._data.get("head")
+
+    @head.setter
+    def head(self, value):
+        """Set head version directory."""
+        self._data["head"] = value
+
+    @property
+    def content_directory(self):
+        """Get contentDirectory."""
+        return self._data.get("contentDirectory")
+
+    @property
+    def content_directory_to_use(self):
+        """Get contentDirectory to use, default 'content' is not specified."""
+        return self._data.get("contentDirectory", "content")
+
+    @content_directory.setter
+    def content_directory(self, value):
+        """Set the contentDirectory."""
+        self._data["contentDirectory"] = value
+
+    @property
+    def manifest(self):
+        """Get the manifest of digests and corresponding content paths.
+
+        Returns dict of digest -> [file paths], an empty dict is there
+        is no manifest.
+        """
+        return self._data.get("manifest", {})
+
+    @property
+    def manifest_add_if_not_present(self):
+        """Get the manifest of digests and corresponding content paths.
+
+        Returns dict of digest -> [file paths]. As a side effect will create
+        an empty dict in the data structure if none was present, so that new
+        data can be added.
+        """
+        if "manifest" not in self._data:
+            self._data["manifest"] = {}
+        return self._data["manifest"]
+
+    @property
+    def content(self):
+        """Get all the content paths and their digests stored within the object.
+
+        Returns a dictionary of content paths with values that are the
+        digests for each file. Essentially an inversion of the manifest.
+        """
+        files = {}
+        for digest in self.manifest:
+            for file in self.manifest[digest]:
+                files[file] = digest
+        return files
+
+    @property
+    def content_paths(self):
+        """Get all the content paths.
+
+        Returns a list of content paths for all files in the object. Will
+        be and empty list if there is no content.
+        """
+        paths = []
+        for files in self.manifest.values():
+            paths += files
+        return paths
+
+    @property
+    def versions(self):
+        """Dict of the versions block.
+
+        Returns a dict whether or not there is a versions block in the
+        underlying data.
+        """
+        return self._data.get("versions", {})
+
+    @property
+    def version_directories(self):
+        """List of all version directories.
+
+        Returns a list of all version in the versions block. The values
+        in the list are the version directory names so they will have
+        the "v" prefix and may or may not be zero padded.
+
+        See also: inv.version_numbers for just the numbers: [1, 2, 3].
+        """
+        return list(self.versions.keys())
+
+    @property
+    def version_numbers(self):
+        """List of all version numbers as integers."""
+        vnums = []
+        for vdir in self.version_directories:
+            vnums.append(version_number_from_directory(vdir))
+        return vnums
+
+    def digest_for_content_path(self, path):
+        """Return digest corresponding to specified content path.
+
+        Returns None if the content path is not specified in the
+        manifest, else the path string.
+        """
+        for digest, paths in self.manifest.items():
+            if path in paths:
+                return digest
+        return None
+
+    def version_data(self, vdir):
+        """Return data for the version in vdir.
+
+        Returns a dict whether or not any data exists.
+        """
+        return self.versions.get(vdir, {})
+
+    def version(self, vdir):
+        """Version object for the specified version directory."""
+        return Version(self, vdir)
+
+    def content_paths_for_digest(self, digest):
+        """Content paths for the given digest.
+
+        Returns a list of content paths or and empty list if there
+        isn't one for the given digest. The list is used because there
+        may be more than one content path for the given digest.
+        """
+        return self.manifest.get(digest, [])
+
+    def content_path_for_digest(self, digest):
+        """Content path for the given digest.
+
+        Returns a content path or None if there isn't one for the
+        given digest. There may actually be more than one content
+        path for the given digest, we return the first in the
+        underlying data.
+        """
+        paths = self.content_paths_for_digest(digest)
+        if len(paths) > 0:
+            return paths[0]
+        return None
+
+    def add_version(self, vdir=None):
+        """Add new version object to the versions block.
+
+        Arguments:
+            vdir: string with the version directory name (e.g. "v1"). If None
+                then will create the next version in sequence
+
+        Returns a Version object to access version properties.
+        """
+        highest_version = 0
+        for vvdir in self.version_directories:
+            highest_version = max(highest_version,
+                                  version_number_from_directory(vvdir))
+        # FIXME - Need to deal with zero padding
+        vdir = "v" + str(highest_version + 1)
+        if "versions" not in self._data:
+            self._data["versions"] = {}
+        self._data["versions"][vdir] = {}
+        self._data["head"] = vdir
+        return self.version(vdir)
+
+    def add_file(self, *, digest, content_path):
+        """Add file to the object in the manifest.
+
+        Arguments:
+            digest: the digest string computed with the specified digest
+                algorithm.
+            content_path: the full content path including version directory,
+                content directory, and the path with the content directory.
+
+        FIXME - Does not check vdir and content_directory
+        """
+        if content_path in self.content_paths:
+            raise InventoryException("Attempt to add a content path that already exists: %s" % content_path)
+        # Does this digest already exist?
+        if digest in self.manifest_add_if_not_present:
+            # Yes, add to file list
+            self.manifest[digest].append(content_path)
+        else:
+            # No, new manifest etry
+            self.manifest[digest] = [content_path]
+
+    def as_json(self):
+        """Serlialize JSON representation."""
+        return json.dumps(self._data, sort_keys=True, indent=2)
+
+
+class Version():
+    """Version class to represent version information in an Inventory.
+
+    The class stores only pointers to the appropriate inventory and
+    version directory with the versions block. These are used to access
+    data in the inventory.
+    """
+
+    def __init__(self, inv, vdir):
+        """Initialize Version object.
+
+        Arguments:
+            inv: Inventory object for which this is one version.
+            vdir: Version directory string for the version of interest.
+        """
+        self.inv = inv
+        self.vdir = vdir
+
+    @property
+    def created(self):
+        """Created string for this version."""
+        return self.inv.version_data(self.vdir).get("created")
+
+    @created.setter
+    def created(self, value):
+        """Set created string for this version."""
+        self.inv.version_data(self.vdir)["created"] = value
+
+    @property
+    def message(self):
+        """Message string for this verion."""
+        return self.inv.version_data(self.vdir).get("message")
+
+    @message.setter
+    def message(self, value):
+        """Set message string for this version."""
+        self.inv.version_data(self.vdir)["message"] = value
+
+    @property
+    def state(self):
+        """State block for this version.
+
+        Returns a dict for the state block or and empty dict if
+        there is no state block.
+        """
+        return self.inv.version_data(self.vdir).get("state", {})
+
+    @property
+    def state_add_if_not_present(self):
+        """State block for this version.
+
+        Returns a dict for the state block.As a side effect will create
+        an empty dict in the data structure if none was present, so that new
+        data can be added.
+        """
+        if "state" not in self.inv.version_data(self.vdir):
+            self.inv.version_data(self.vdir)["state"] = {}
+        return self.inv.version_data(self.vdir)["state"]
+
+    @property
+    def user(self):
+        """User block for this version."""
+        return self.inv.version_data(self.vdir).get("user", {})
+
+    @property
+    def user_add_if_not_present(self):
+        """User block for this version, add if not present."""
+        if "user" not in self.inv.version_data(self.vdir):
+            self.inv.version_data(self.vdir)["user"] = {}
+        return self.inv.version_data(self.vdir)["user"]
+
+    @property
+    def user_address(self):
+        """Address element in the user description for this version.
+
+        Returns a string or None is there is no name.
+        """
+        return self.user.get("address")
+
+    @user_address.setter
+    def user_address(self, value):
+        """Set address element in the user description for this version."""
+        self.user_add_if_not_present["address"] = value
+
+    @property
+    def user_name(self):
+        """Name element in the user description for this version.
+
+        Returns a string or None is there is no name.
+        """
+        return self.user.get("name")
+
+    @user_name.setter
+    def user_name(self, value):
+        """Set name element in the user description for this version."""
+        self.user_add_if_not_present["name"] = value
+
+    @property
+    def logical_paths(self):
+        """List of the logical paths in this version.
+
+        Returns a list, will be empty if there are no logical paths.
+        """
+        paths = []
+        for files in self.state.values():
+            paths += files
+        return paths
+
+    def digest_for_logical_path(self, path):
+        """Digest for the given logical path in this version.
+
+        Return digest string or None is path not found.
+        """
+        for digest, paths in self.state.items():
+            if path in paths:
+                return digest
+        return None
+
+    def content_path_for_logical_path(self, path):
+        """Content path for the file corresponding with the logical path.
+
+        Note that there could be more than one content path with the
+        digest. Here we just return the first one. Returns None if there
+        no matching content path.
+        """
+        digest = self.digest_for_logical_path(path)
+        if digest is None:
+            return None
+        return self.inv.content_path_for_digest(digest)
+
+    def add_file(self, *, digest, logical_path, content_path=None):
+        """Add information for a file with given digest in this version.
+
+        Arguments:
+            digest: the file digest that must be created using the
+                appropriate digestAlgorithm.
+            logical_path: path within the state for this version.
+            content_path: local content path to be used if a file is created
+                within this version. If not specified then the logical_path name
+                will be used as the basis.
+
+        Returns the full content path (including vdir and content directory) of
+        the file added, else None if no file was added.
+        """
+        # Check logical_file not already present
+        if logical_path in self.logical_paths:
+            raise InventoryException("Logical path already exists in this version: %s" % logical_path)
+        # Do we have any files with this digest already?
+        files = self.inv.content_paths_for_digest(digest)
+        if len(files) == 0:
+            # No such file exists, add it
+            suggested = os.path.join(self.vdir,
+                                     self.inv.content_directory_to_use,
+                                     logical_path if content_path is None else content_path)
+            content_path = suggested
+            n = 1
+            while content_path in self.inv.manifest.values():
+                n += 1
+                content_path = suggested + "_" + str(n)
+            # Have location now, add to manifest
+            self.inv.add_file(digest=digest, content_path=content_path)
+        else:
+            # File or files with same digest exist
+            content_path = None
+        # Now add to the version state
+        if digest in self.state_add_if_not_present:
+            self.state[digest].append(logical_path)
+        else:
+            self.state[digest] = [logical_path]
+        return content_path
---
+++ tests/test_inventory.py
@@ -0,0 +1,57 @@
+"""Inventory tests."""
+import unittest
+
+from ocfl.inventory import InventoryException, Inventory
+
+
+class TestAll(unittest.TestCase):
+    """TestAll class to run tests."""
+
+    def test_init(self):
+        """Test initialization."""
+        self.assertRaises(InventoryException, Inventory, data=[])
+
+    def test_getter_properties_minimal_example(self):
+        """Test read of fixture and extract properties."""
+        inv = Inventory(filepath="fixtures/1.1/good-objects/minimal_one_version_one_file/inventory.json")
+        self.assertEqual(inv.spec_version, "1.1")
+        self.assertEqual(inv.digest_algorithm, "sha512")
+        self.assertEqual(inv.identifier, "ark:123/abc")
+        self.assertEqual(inv.head, "v1")
+        self.assertEqual(inv.content_paths, ["v1/content/a_file.txt"])
+        self.assertEqual(inv.digest_for_content_path("v1/content/a_file.txt"),
+                         "43a43fe8a8a082d3b5343dfaf2fd0c8b8e370675b1f376e92e9994612c33ea255b11298269d72f797399ebb94edeefe53df243643676548f584fb8603ca53a0f")
+        self.assertEqual(inv.version_directories, ["v1"])
+        self.assertEqual(inv.version_numbers, [1])
+        self.assertEqual(inv.version("v1").created,
+                         "2019-01-01T02:03:04Z")
+        self.assertEqual(inv.version("v1").message,
+                         "An version with one file")
+        self.assertEqual(inv.version("v1").user_name,
+                         "A Person")
+        self.assertEqual(inv.version("v1").user_address,
+                         "mailto:a_person@example.org")
+        self.assertEqual(inv.version("v1").logical_paths, ["a_file.txt"])
+        self.assertEqual(inv.version("v1").digest_for_logical_path("a_file.txt"),
+                         "43a43fe8a8a082d3b5343dfaf2fd0c8b8e370675b1f376e92e9994612c33ea255b11298269d72f797399ebb94edeefe53df243643676548f584fb8603ca53a0f")
+        self.assertEqual(inv.version("v1").content_path_for_logical_path("a_file.txt"),
+                         "v1/content/a_file.txt")
+
+    def test_creation_of_minimal_example(self):
+        """Test creation of minimal example."""
+        inv = Inventory()
+        inv.spec_version = "1.1"
+        inv.digest_algorithm = "sha512"
+        inv.identifier = "ark:123/abc"
+        v = inv.add_version()
+        v.created = "2019-01-01T02:03:04Z"
+        v.message = "An version with one file"
+        v.user_name = "A Person"
+        v.user_address = "mailto:a_person@example.org"
+        v.add_file(digest="43a43fe8a8a082d3b5343dfaf2fd0c8b8e370675b1f376e92e9994612c33ea255b11298269d72f797399ebb94edeefe53df243643676548f584fb8603ca53a0f",
+                   logical_path="a_file.txt")
+        json1 = inv.as_json()
+        # print("###json1###\n" + json1)
+        json2 = Inventory(filepath="fixtures/1.1/good-objects/minimal_one_version_one_file/inventory.json").as_json()
+        # print("###json2###\n" + json2)
+        self.assertEqual(json1, json2)
